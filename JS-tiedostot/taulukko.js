57a7e470569b3ee932ccb1e2e643452c406667bf
Saarinen Ville <ville.saarinen@edu.keuda.fi>
2022-09-14 09:47:59 +0300

    js

---
+++ JS-tiedostot/taulukko.js
@@ -0,0 +1,10 @@
+function esimerkkiTaulukko()
+{
+  var autot = [];
+  var caarat = ["Tesla", "VW i3", "Hyundai ionic"];
+  autot.push("Jeep");
+  document.write(caarat[1] + " " +autot[0] + "<br />");
+  document.write(caarat + "<br />");
+  document.write(autot);
+
+}
