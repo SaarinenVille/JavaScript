3ee093ce1ff36072ea9ca825fa4e79b520e469ed
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-09-25 19:44:37 +0300

    Viikko 2 toiset harjoitukset

--- JS-tiedostot/viikko2_harjoitukset/toiset.js
+++ JS-tiedostot/viikko2_harjoitukset/toiset.js
@@ -1,3 +1,4 @@
+// Harjoitus 1 - Tarkistaa, onko luku parillinen vai pariton
 function tarkista()
 {
 var numero;
@@ -11,4 +12,71 @@ else
 {
   document.write("Luku on negatiivininen");
 }
+}
+
+// Harjoitus 2 - Muuttaa viikonpäivät luvusta nimeksi
+function viikonpaiva()
+{
+
+var viikonp = parseInt(document.getElementById('paiva').value);
+switch (viikonp)
+{
+  case 1:
+  document.write("Viikonpäivä 1 on maanantai");
+  break;
+
+  case 2:
+  document.write("Viikonpäivä 2 on tiistai");
+  break;
+
+  case 3:
+  document.write("Viikonpäivä 3 on keskiviikko");
+  break;
+
+  case 4:
+  document.write("Viikonpäivä 4 on torstai");
+  break;
+
+  case 5:
+  document.write("Viikonpäivä 5 on perjantai");
+  break;
+
+  case 6:
+  document.write("Viikonpäivä 6 on lauantai");
+  break;
+
+  case 7:
+  document.write("Viikonpäivä 7 on sunnuntai");
+  break;
+
+  default:
+  document.write("Syötit jotain muuta.")
+  break;
+}
+}
+
+// Harjoitus 3 - Määtittää onko annettu vuosi karkausvuosi
+function karkausvuosi()
+{
+var vuosiluku = parseInt(document.getElementById('vuosi').value);
+
+if (vuosiluku%4 == 0 && vuosiluku%100 != 0)
+{
+  document.write("Vuosi on karkausvuosi");
+}
+else if (vuosiluku%400 == 0)
+{
+  document.write("Vuosi on karkausvuosi");
+}
+else
+{
+  document.write("Vuosi ei ole karkausvuosi");
+}
+
+
+
+
+
+
+
 }
