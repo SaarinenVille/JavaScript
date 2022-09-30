14dcf8d4dd2ad185129c548c048aa48bc8a5cad1
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-01 00:32:41 +0300

    Vuokaavio testit

---
+++ JS-tiedostot/Vuokaaviot/harjoitus1.js
@@ -0,0 +1,50 @@
+function secondsOnly(h, m, s) {
+  var sekunnit = h*3600 + m*60 + s;
+
+  document.write(sekunnit + " sekuntia");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function secondsOnly() {
+
+var hours = document.getElementById('tunnit').value;
+var minutes = document.getElementById('tunnit').value;
+var seconds = document.getElementById('tunnit').value;
+
+var onlySeconds = hours*3600 + minutes*60 + seconds;
+
+document.write(onlySeconds + " sekuntia");
+
+
+
+
+}
+
+*/
