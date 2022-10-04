ddf0ff5bda73f8dc0fe8131f8fc2b4b731c63522
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-04 18:08:28 +0300

    viikko 3

--- JS-tiedostot/viikko3_harjoitukset/ensimmäiset/script.js
+++ JS-tiedostot/viikko3_harjoitukset/ensimmäiset/script.js
@@ -8,22 +8,3 @@ function parillinen() {
   }
   document.getElementById('parilliset').innerHTML = tulosta;
 }
-
-
-
-
-
-
-
-function tulostaParilliset()
-{
-  var karvo = parseInt(document.getElementById('pluku').value);
-  var jono = "<p>";
-  for(var i = 2; i <= karvo; i= i+2)
-  {
-    jono+=i;
-    jono+=" ";
-  }
-  jono+="</p>";
-    document.getElementById('parilliset').innerHTML = jono;
-}
