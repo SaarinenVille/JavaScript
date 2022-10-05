1538ccf8f004ed6da5760e691a0f603f7c3c291f
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-05 17:30:34 +0300

    Viikko 3 ensimmäiset

--- JS-tiedostot/viikko3_harjoitukset/ensimmäiset/script.js
+++ JS-tiedostot/viikko3_harjoitukset/ensimmäiset/script.js
@@ -8,3 +8,65 @@ function parillinen() {
   }
   document.getElementById('parilliset').innerHTML = tulosta;
 }
+
+function salasanaksi() {
+
+var sana = document.getElementById('sana').value;
+var salasana = "";
+
+for(var j = 0; j < sana.length; j++) {
+  salasana += sana[j] + "Ö";
+}
+salasana += ""
+document.getElementById('salasana').innerHTML = salasana;
+}
+
+function onkoKirjainta() {
+
+  const sana = document.getElementById('t3_syotto').value;
+  var vastaus = "Ei ole";
+
+  for(var k = 0; k < sana.length; k++) {
+    if(sana[k] == "Ö" || sana[k] == "ö")
+    {
+      vastaus = "On";
+    }
+  }
+  document.getElementById('onko').innerHTML = vastaus;
+}
+
+function kertoma() {
+
+  var numero = parseInt(document.getElementById('t4_syotto').value);
+  var vast = 1;
+
+  for(var l = 1; l <= numero; l++) {
+    vast *= l;
+  }
+  document.getElementById('t4_vastaus').innerHTML = vast;
+}
+
+function hipHeijaa() {
+  var lukujono = "";
+
+  for(var z = 1; z <= 100; z++)
+  {
+    if(z % 3 == 0 && z % 5 == 0)
+    {
+      lukujono += " hip heijaa ";
+    }
+    else if(z % 5 == 0)
+    {
+      lukujono += " heijaa ";
+    }
+    else if (z % 3 == 0)
+    {
+      lukujono += " hip ";
+    }
+    else
+    {
+      lukujono += z + " ";
+    }
+  }
+  document.getElementById('t5_vastaus').innerHTML = lukujono;
+}
