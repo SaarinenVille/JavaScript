2846da509a43e099ee80211893c2fdc2870e5476
Ville Saarinen <ville.saarinen@edu.keuda.fi>
2022-10-07 16:02:44 +0300

    Viikko 3 tehtävät 2

--- JS-tiedostot/viikko3_harjoitukset/toiset/toiset.js
+++ JS-tiedostot/viikko3_harjoitukset/toiset/toiset.js
@@ -91,3 +91,18 @@ document.getElementById('t2_vastaus').innerHTML = numerot.toString();
 function randomInt(max) {
     return Math.floor((Math.random() * max) + 1);
 }
+
+function taulukko() {
+
+  var luvut = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]]
+  var table = '<table border="1">';
+
+  for(var i = 0; i < luvut.length; i++) {
+    table += '<tr>';
+    for(var j = 0; j < luvut[i].length; j++) {
+      table += '<td>' + luvut[i][j];
+    }
+  }
+  table += '</table>';
+  document.getElementById('t3_vastaus').innerHTML = table;
+}
